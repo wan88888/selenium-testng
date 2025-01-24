c5cf730241298309ef70fa2c378fac43731500a5
wan88888 <wy13076768756@163.com>
2025-01-24 15:14:46 +0800

    update1

--- src/main/java/com/example/core/CustomWebDriverManager.java
+++ src/main/java/com/example/core/CustomWebDriverManager.java
@@ -3,6 +3,7 @@ package com.example.core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CustomWebDriverManager {
@@ -17,7 +18,11 @@ public class CustomWebDriverManager {
                 break;
             default:
                 io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                driver = new ChromeDriver(options);
         }
         driver.manage().window().maximize();
         driverThreadLocal.set(driver);
